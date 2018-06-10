a3e6d5a71fddd94bee0a1a09a06d53f99fba734e
Rudie Dirkx <github@hotblocks.nl>
2018-06-10 13:37:43 +0200

    +whitespace demo

---
+++ whitespace.php
@@ -0,0 +1,17 @@
+<?php
+
+use Twig\Environment;
+use Twig\Node\Node;
+use Twig\Token;
+use Twig\TokenParser\AbstractTokenParser;
+
+list($twigMaker) = require 'inc.bootstrap.php';
+
+header('Content-type: text/plain; charset=utf-8');
+
+$twig = $twigMaker();
+$template = $twig->load("tpl.whitespace.twig");
+$output = $template->render(['last_run' => rand(0, 1) ? date('Y-m-d') : null]);
+
+header('Content-type: text/html; charset=utf-8');
+echo $output;
